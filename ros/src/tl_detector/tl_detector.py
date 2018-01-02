cc4c89acc1289f5cd35793e7d4e95caa3e6e5a26
M0J01 <moodymusicman@gmail.com>
2018-01-02 06:43:06 -0500

    Made typo using tf instead of tfl

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -37,11 +37,11 @@ NUM_CLASSES = 4
 ### Load Frozen Graph
 detection_graph = tfl.Graph()
 with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_FROZEN_MODEL, 'rb') as fid:
+    od_graph_def = tfl.GraphDef()
+    with tfl.gfile.GFile(PATH_TO_FROZEN_MODEL, 'rb') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
+        tfl.import_graph_def(od_graph_def, name='')
 
 
 
