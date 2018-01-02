080043fdcf7a1fa13b74cb0d38008b6cff073119
M0J01 <moodymusicman@gmail.com>
2018-01-02 06:17:35 -0500

    Adding utilities to import

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -24,6 +24,9 @@ from io import StringIO
 from matplotlib import pyplot as plt
 #from PIL import Image
 
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
 
 STATE_COUNT_THRESHOLD = 3
 
