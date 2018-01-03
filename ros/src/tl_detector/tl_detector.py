546c2c2d08ad264827b3980c2286a18ae1692c13
M0J01 <moodymusicman@gmail.com>
2018-01-03 11:23:28 -0500

    Finding index of max score

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -167,8 +167,8 @@ class TLDetector(object):
                 #rospy.logerr('scores:')
                 #rospy.logerr(scores)
                 rospy.logerr(time.time() - time1)
-                rospy.logerr(" The number of detections: {}".format(max(classes[0])))
-                rospy.logerr(" The number of scores: {}".format(max(scores[0])))
+                rospy.logerr(" The number of detections: {}".format(classes[0].index(max(classes[0]))))
+                rospy.logerr(" The number of scores: {}".format(scores.index(max(scores[0]))))
 
 
 
