a578fd17083af4134a4eeae2487bfd6a951a8988
Parallels <sven.bone@mail.de>
2018-01-07 15:06:33 +0100

    Added TL detector debug image with overlay which can be activated in the launch file. Fixed double model loading.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -5,7 +5,7 @@ from geometry_msgs.msg import PoseStamped, Pose
 from styx_msgs.msg import TrafficLightArray, TrafficLight
 from styx_msgs.msg import Lane
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 from light_classification.tl_classifier import TLClassifier
 import cv2
 import yaml
@@ -63,6 +63,17 @@ class TLDetector(object):
         self.camera_image = None
         self.lights = []
 
+	# get parameters from launch file
+        self.tl_show_detector_results = rospy.get_param('debug_tl_detector', False)
+        
+        if self.tl_show_detector_results is True:
+            rospy.logwarn('TL detector debugging activated (debug_tl_detector = {}). Please deactivate \
+                           it in the launch files for real-time performance.'.format(self.tl_show_detector_results))
+            self.bridge = CvBridge()
+            self.pub_image_tl_overlay = rospy.Publisher('/image_tl_overlay', Image, queue_size=1)
+        else:
+            rospy.loginfo('TL detector debugging disabled. You can activate it in the launch files.')
+
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
@@ -91,6 +102,22 @@ class TLDetector(object):
         self.state_count = 0
         self.state_average = [0,0,0,0]
         self.model_loaded = False
+        
+        # Load Model
+        if (self.model_loaded == False):
+            rospy.loginfo('Loading the TL model...') 
+            model_path = PATH_TO_REAL_FROZEN_MODEL
+            label_path = PATH_TO_REAL_LABELS
+ 
+            with detection_graph.as_default():
+                od_graph_def = tfl.GraphDef()
+                with tfl.gfile.GFile(model_path, 'rb') as fid:
+                    serialized_graph = fid.read()
+                    od_graph_def.ParseFromString(serialized_graph)
+                    tfl.import_graph_def(od_graph_def, name='')
+
+            self.model_loaded = True
+            rospy.loginfo('TL model loaded.') 
 
         rospy.spin()
 
@@ -111,33 +138,9 @@ class TLDetector(object):
             msg (Image): image from car-mounted camera
 
         """
-
-        # Load Model
-        if (self.model_loaded == False):
-            #rospy.logwarn(msg.height)   # Height is 600
-            #rospy.logwarn(msg.width)    # Width is 800
-            #thaw_Model(PATH_TO_SIM_FROZEN_MODEL, PATH_TO_SIM_LABELS)
-            ### Load Frozen Graph
-
-            # Determine which model to load
-            if msg.height == 600 and msg.width == 800:
-                model_path = PATH_TO_SIM_FROZEN_MODEL
-                label_path = PATH_TO_SIM_LABELS
-            else:
-                model_path = PATH_TO_REAL_FROZEN_MODEL
-                label_path = PATH_TO_REAL_LABELS
-                #rospy.logwarn("Carla")
-
-            with detection_graph.as_default():
-                od_graph_def = tfl.GraphDef()
-                with tfl.gfile.GFile(model_path, 'rb') as fid:
-                    serialized_graph = fid.read()
-                    od_graph_def.ParseFromString(serialized_graph)
-                    tfl.import_graph_def(od_graph_def, name='')
-
-
-            self.model_loaded = True
-
+        
+        if self.model_loaded is False:
+            return
 
         self.has_image = True
         self.camera_image = msg
@@ -160,14 +163,27 @@ class TLDetector(object):
                 (boxes, scores, classes, num) = sess.run(
                     [detection_boxes, detection_scores, detection_classes, num_detections],
                     feed_dict={image_tensor: image_np_expanded})
+                delta_time = time.time() - time1
                 #rospy.logerr('Number of detections: {}'.format(num))
                 #rospy.logerr('Classes:')
                 #rospy.logerr(classes)
                 #rospy.logerr('scores:')
                 #rospy.logerr(scores)
-                #rospy.logwarn('Time for model prediction: {}'.format(time.time() - time1))
 
+                if delta_time > 1.0:
+                    rospy.logwarn('Time for model prediction > 1.0 sec: {} s'.format(delta_time))
 
+                # Publish TL overlay image for debgging if activated in the launch file
+                if self.tl_show_detector_results is True:
+                    rospy.loginfo('Top 10 Classes: {}'.format(classes[0, 0:10]))
+                    rospy.loginfo('Top 10 Scores: {}'.format(scores[0, 0:10]))
+                    image_np = self.draw_bounding_boxes(image_np, boxes, scores, classes, min_score_thresh=0.5, line_thickness=2)
+               
+                    try:
+                        self.pub_image_tl_overlay.publish(self.bridge.cv2_to_imgmsg(image_np, "rgb8"))
+                    except CvBridgeError as e:
+                        print(e)
+               
         # Grab the class with the heighest prediction score
         # 1 = Undefined, 2 = Red, 3 = Yellow, 4 = Green
         score = scores[0][np.argmax(scores)]
@@ -245,6 +261,52 @@ class TLDetector(object):
             return light_wp, state
         self.waypoints = None
         return -1, TrafficLight.UNKNOWN
+
+    def draw_bounding_boxes(self, image, boxes, scores, classes, min_score_thresh=0.5, line_thickness=4):
+        """ Draws the bounding boxes as overlay onto the RGB image.
+
+        :param image:            RGB numpy image.
+        :param boxes:            List of bounding boxes.
+        :param scores:           Detection scores.
+        :param min_score_thresh: Minimum score threshold for a box to be visualized. If None draw all boxes.
+        :param line_thickness:   Thickness of bounding box lines.
+        :param classes:          Traffic light classes (1=undefined, 2=red, 3=yellow, 4=green)
+
+        :return: Image with bounding box overlay.
+        """
+        height, width, _ = image.shape
+
+        for i in range(len(boxes)):
+            for n in range(len(boxes[i])):
+                if min_score_thresh is None or scores[i][n] >= min_score_thresh:
+                    color = (100, 100, 100)
+                    text = 'TL_undefined'
+
+                    if classes[i][n] == 2:
+                        color = (255, 0, 0)
+                        text = 'TL_red'
+                    elif classes[i][n] == 3:
+                        color = (255, 255, 0)
+                        text = 'TL_yellow'
+                    elif classes[i][n] == 4:
+                        color = (0, 255, 0)
+                        text = 'TL_green'
+
+                    text = '{} {:.1f}%'.format(text, scores[i][n] * 100.0)
+                    x_max = int(round(boxes[i][n][3] * width))
+                    x_min = int(round(boxes[i][n][1] * width))
+                    y_max = int(round(boxes[i][n][2] * height))
+                    y_min = int(round(boxes[i][n][0] * height))
+
+                    cv2.rectangle(image, (x_min, y_min), (x_max, y_max), color, thickness=line_thickness)
+                    cv2.putText(image, text,
+                                (x_min, y_min - line_thickness - 1),
+                                cv2.FONT_HERSHEY_DUPLEX,
+                                fontScale=0.4,
+                                color=color,
+                                lineType=1)
+        return image
+
 
 if __name__ == '__main__':
     try:
