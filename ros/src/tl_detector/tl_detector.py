7571c1c2c6de2a1da70aefab81157eab31fe06a1
Parallels <sven.bone@mail.de>
2018-01-09 23:10:30 +0100

    Moved TL detector into a separate thread. Speed-up sess.run() block. Removed unused code.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -18,21 +18,15 @@ import six.moves.urllib as urllib
 import sys
 import tarfile
 import zipfile
+import threading
 
 from collections import defaultdict
 from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image as Img
 
-
-# Used in Label Mapping
-#from google.protobuf import text_format
-import logging
-
-#PATH_TO_FROZEN_MODEL = "/home/student/frozen_inference_graph.pb"
 PATH_TO_SIM_FROZEN_MODEL = "../frozen_inference_graph.pb"
 PATH_TO_REAL_FROZEN_MODEL = "../frozen_inference_graph.pb"
-
 
 PATH_TO_SIM_LABELS = "../traffic_light_label_map.pbtxt"
 PATH_TO_REAL_LABELS = "../traffic_light_label_map.pbtxt"
@@ -41,27 +35,13 @@ NUM_CLASSES = 4
 STATE_COUNT_THRESHOLD = 3
 
 
-model_path = PATH_TO_SIM_FROZEN_MODEL
-label_path = PATH_TO_SIM_LABELS
-
-detection_graph = tfl.Graph()
-
-# Image Helper Code
-def load_image_into_numpy_array(image):
-    bridge = CvBridge()
-    cv_image = bridge.imgmsg_to_cv2(image, "rgb8")  #Format that has worked the best so far. bgr8 did not work
-    im_width = image.width
-    im_height = image.height
-    return np.array(cv_image).reshape(im_height, im_width, 3).astype(np.uint8)
-
 class TLDetector(object):
+    """ Traffic Light Detector Node. """
+    
     def __init__(self):
+        """ Initialization. """
+        
         rospy.init_node('tl_detector')
-
-        self.pose = None
-        self.waypoints = None
-        self.camera_image = None
-        self.lights = []
 
         # get parameters from launch file
         self.tl_show_detector_results = rospy.get_param('debug_tl_detector', False)
@@ -73,63 +53,49 @@ class TLDetector(object):
             self.pub_image_tl_overlay = rospy.Publisher('/image_tl_overlay', Image, queue_size=1)
         else:
             rospy.loginfo('TL detector debugging disabled. You can activate it in the launch files.')
-
-        sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-
-        '''
-        /vehicle/traffic_lights provides you with the location of the traffic light in 3D map space and
-        helps you acquire an accurate ground truth data source for the traffic light
-        classifier by sending the current color state of all traffic lights in the
-        simulator. When testing on the vehicle, the color state will not be available. You'll need to
-        rely on the position of the light and the camera image to predict it.
-        '''
-        sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
-        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb, queue_size = 1, buff_size=2**24)
-
-        config_string = rospy.get_param("/traffic_light_config")
-        self.config = yaml.load(config_string)
-
+        
+        sub6 = rospy.Subscriber('/image_raw', Image, self.image_cb, queue_size = 1, buff_size=2**24)
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
+        self.camera_image = None
         self.bridge = CvBridge()
-        self.light_classifier = TLClassifier()
-        self.listener = tf.TransformListener()
 
-        self.processing = False
         self.state = -1
         self.last_state = -2
-        self.last_wp = -1
         self.state_count = 0
         self.state_average = [0,0,0,0]
-        self.model_loaded = False
         
-        # Load Model
-        if (self.model_loaded == False):
-            rospy.loginfo('Loading the TL model...') 
-            model_path = PATH_TO_REAL_FROZEN_MODEL
-            #label_path = PATH_TO_REAL_LABELS   #Only used in model training, not needed for implementation
- 
-            with detection_graph.as_default():
-                od_graph_def = tfl.GraphDef()
-                with tfl.gfile.GFile(model_path, 'rb') as fid:
-                    serialized_graph = fid.read()
-                    od_graph_def.ParseFromString(serialized_graph)
-                    tfl.import_graph_def(od_graph_def, name='')
+        # Load Model and initialize tensorflow
+        rospy.loginfo('Loading the TL model...') 
+        model_path = PATH_TO_REAL_FROZEN_MODEL
+        self.detection_graph = tfl.Graph()
+    
+        with self.detection_graph.as_default():
+            od_graph_def = tfl.GraphDef()
+            with tfl.gfile.GFile(model_path, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tfl.import_graph_def(od_graph_def, name='')
+            self.sess = tfl.Session(graph=self.detection_graph)
 
-            self.model_loaded = True
-            rospy.loginfo('TL model loaded.')
+        rospy.loginfo('TL model loaded.')
 
+        # run tl detector thread
+        rospy.loginfo('Start TL detector thread...')
+        self.event_has_image = threading.Event()
+        self.lock_image = threading.Lock()
+        tl_thread = threading.Thread(target=self.process_image)
+        tl_thread.start()
+        rospy.loginfo('TL detector up and running.')
+        
         rospy.spin()
 
-    def pose_cb(self, msg):
-        self.pose = msg
-
-    def waypoints_cb(self, waypoints):
-        self.waypoints = waypoints
-
-    def traffic_cb(self, msg):
-        self.lights = msg.lights
+    def load_image_into_numpy_array(self, image):
+        """ Image converter ROS Image to numpy array. """
+        cv_image = self.bridge.imgmsg_to_cv2(image, "rgb8")
+        im_width = image.width
+        im_height = image.height
+        return np.array(cv_image).reshape(im_height, im_width, 3).astype(np.uint8)
 
     def image_cb(self, msg):
         """Identifies red lights in the incoming camera image and publishes the index
@@ -139,56 +105,57 @@ class TLDetector(object):
             msg (Image): image from car-mounted camera
 
         """
-
-        self.has_image = True
+        
+        self.lock_image.acquire()
         self.camera_image = msg
-        self.process_image()
-
+        self.lock_image.release()
+        self.event_has_image.set()
 
     def process_image(self):
-        ### Perform Model Prediction
-
-        if self.has_image == True:
+        """" Perform Model Prediction. """
+        
+        while not rospy.is_shutdown():
+            # wait for valid image to process
+            if not self.event_has_image.wait(1):
+                continue
+            
+            self.lock_image.acquire()
             image = self.camera_image
-            image_np = load_image_into_numpy_array(image)
+            self.lock_image.release()
+            image_np = self.load_image_into_numpy_array(image)
             image_np_expanded = np.expand_dims(image_np, axis=0)
 
-            with detection_graph.as_default():
-                with tfl.Session(graph=detection_graph) as sess:
-                    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-                    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-                    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-                    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-                    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+            image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+            detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
 
-                    # Perform Detection
-                    time1 = time.time()
-                    (boxes, scores, classes, num) = sess.run(
-                        [detection_boxes, detection_scores, detection_classes, num_detections],
-                        feed_dict={image_tensor: image_np_expanded})
-                    delta_time = time.time() - time1
-                    #rospy.logerr('Number of detections: {}'.format(num))
-                    #rospy.logerr('Classes:')
-                    #rospy.logerr(classes)
-                    #rospy.logerr('scores:')
-                    #rospy.logerr(scores)
+            # Perform Detection
+            time1 = time.time()
+            (boxes, scores, classes, num) = self.sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+            delta_time = time.time() - time1
+            #rospy.logerr('Number of detections: {}'.format(num))
+            #rospy.logerr('Classes:')
+            #rospy.logerr(classes)
+            #rospy.logerr('scores:')
+            #rospy.logerr(scores)
 
-                    if delta_time > 1.0:
-                        rospy.logwarn('Time for model prediction > 1.0 sec: {} s'.format(delta_time))
+            if delta_time > 1.0:
+                rospy.logwarn('Time for model prediction > 1.0 sec: {} s'.format(delta_time))
 
-                    # Publish TL overlay image for debgging if activated in the launch file
-                    if self.tl_show_detector_results is True:
-                        rospy.loginfo('Top 10 Classes: {}'.format(classes[0, 0:10]))
-                        rospy.loginfo('Top 10 Scores: {}'.format(scores[0, 0:10]))
-                        image_np = self.draw_bounding_boxes(image_np, boxes, scores, classes, min_score_thresh=0.5, line_thickness=2)
+            # Publish TL overlay image for debgging if activated in the launch file
+            if self.tl_show_detector_results is True:
+                rospy.loginfo('Top 10 Classes: {}'.format(classes[0, 0:10]))
+                rospy.loginfo('Top 10 Scores: {}'.format(scores[0, 0:10]))
+                image_np = self.draw_bounding_boxes(image_np, boxes, scores, classes, min_score_thresh=0.5, line_thickness=2)
 
-                        try:
-                            self.pub_image_tl_overlay.publish(self.bridge.cv2_to_imgmsg(image_np, "rgb8"))
-                        except CvBridgeError as e:
-                            print(e)
-
-
-
+                try:
+                    self.pub_image_tl_overlay.publish(self.bridge.cv2_to_imgmsg(image_np, "rgb8"))
+                except CvBridgeError as e:
+                    print(e)
 
             # Grab the class with the heighest prediction score
             # 1 = Undefined, 2 = Red, 3 = Yellow, 4 = Green
@@ -210,63 +177,13 @@ class TLDetector(object):
                 pubmsg = Int32()
                 pubmsg.data = tl_state_prediction
                 self.upcoming_red_light_pub.publish(pubmsg)
-                #rospy.logwarn("Traffic Light State Published: {}".format(tl_state_dict[state]))
-
-    def get_closest_waypoint(self, pose):
-        """Identifies the closest path waypoint to the given position
-            https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
-        Args:
-            pose (Pose): position to match a waypoint to
-
-        Returns:
-            int: index of the closest waypoint in self.waypoints
-
-        """
-        #TODO implement
-        return 0
-
-    def get_light_state(self, light):
-        """Determines the current color of the traffic light
-
-        Args:
-            light (TrafficLight): light to classify
-
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
-        """
-        if(not self.has_image):
-            self.prev_light_loc = None
-            return False
-
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
-
-        #Get classification
-        return self.light_classifier.get_classification(cv_image)
-
-    def process_traffic_lights(self):
-        """Finds closest visible traffic light, if one exists, and determines its
-            location and color
-
-        Returns:
-            int: index of waypoint closes to the upcoming stop line for a traffic light (-1 if none exists)
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
-        """
-        light = None
-
-        # List of positions that correspond to the line to stop in front of for a given intersection
-        stop_line_positions = self.config['stop_line_positions']
-        if(self.pose):
-            car_position = self.get_closest_waypoint(self.pose.pose)
-
-        #TODO find the closest visible traffic light (if one exists)
-
-        if light:
-            state = self.get_light_state(light)
-            return light_wp, state
-        self.waypoints = None
-        return -1, TrafficLight.UNKNOWN
+                #rospy.logwarn("Traffic Light State Published: {}".format(tl_state_dict[state])
+            
+            self.event_has_image.clear()
+        
+        # clean-up tensorflow session
+        self.sess.close()    
+        rospy.loginfo('TL detector thread stopped.')
 
     def draw_bounding_boxes(self, image, boxes, scores, classes, min_score_thresh=0.5, line_thickness=4):
         """ Draws the bounding boxes as overlay onto the RGB image.
@@ -312,10 +229,6 @@ class TLDetector(object):
                                 color=color,
                                 lineType=1)
         return image
-
-    def my_callback(self):
-        rospy.logwarn('Timer called at')
-
 
 
 if __name__ == '__main__':
